d04a06e4241bdfb21454be15f2dc8edbb159cdfc
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2023-01-06 14:48:21 +0300

    Update the production url

--- api-server.js
+++ api-server.js
@@ -9,7 +9,8 @@ const jwksRsa = require('jwks-rsa');
 
 const app = express();
 const port = process.env.API_PORT || 3001;
-const baseUrl = process.env.AUTH0_BASE_URL;
+// const baseUrl = process.env.AUTH0_BASE_URL;
+const baseUrl = 'https://responsive-travel-agency-landing-page-nextjs-tailwindcss.vercel.app'
 const issuerBaseUrl = process.env.AUTH0_ISSUER_BASE_URL;
 const audience = process.env.AUTH0_AUDIENCE;
 
