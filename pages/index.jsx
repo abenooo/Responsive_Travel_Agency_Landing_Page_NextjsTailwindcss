b29fe351084e9f37b5a39c9ddb8ecbfe50410ad2
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2023-01-12 11:44:15 +0300

    Update home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import Notify from '../components/Notify';
+import Services from '../components/Services';
 
 export default function Index() {
   return (
     <>
+    
+    <Notify />
+    <Services />
     </>
   );
 }
